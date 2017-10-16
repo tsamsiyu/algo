ff0cc02ee96a3252f1ca94a6cc2e80a33ef196d8
Dmitry Likhovoi <ts.likhovoi.dim@gmail.com>
2017-10-17 00:38:34 +0300

    merge sort, in process

--- mergeSort1.js
+++ mergeSort1.js
@@ -1,37 +1,45 @@
 module.exports = function (inputs) {
     const inputsSize = inputs.length;
-    let offset = 0;
+    const inputsSizeHalf = inputsSize >> 1;
 
-    for (let splitSize = 1; splitSize <= inputsSize; splitSize *= 2) {
-        const prevOffset = offset;
-        let l = offset;
-        let r = l + splitSize;
-        offset += r + splitSize;
-        const lEnd = r;
-        const rEnd = offset;
-        const mergeSize = splitSize * 2;
-        const merge = new Array(mergeSize);
+    console.log(inputs);
 
-        for (let m = 0; m < mergeSize; m++) {
-            if (r >= rEnd) {
-                merge[m] = inputs[l];
-                l++;
-            } else if (l >= lEnd) {
-                merge[m] = inputs[r];
-                r++;
-            } else {
-                if (inputs[l] > inputs[l]) {
+    for (let splitSize = 1; splitSize <= inputsSizeHalf; splitSize *= 2) {
+        const step = splitSize * 2;
+        for (let offset = 0; offset + step <= inputsSize; offset += step) {
+            // console.log('');
+            let l = offset;
+            let r = l + splitSize;
+            const lEnd = r;
+            const rEnd = r+ splitSize;
+            const mergeSize = splitSize * 2;
+            const merge = new Array(mergeSize);
+
+            // console.log(inputs.slice(l, l+splitSize));
+            // console.log(inputs.slice(r, r+splitSize));
+
+            for (let m = 0; m < mergeSize; m++) {
+                if (r >= rEnd) {
+                    merge[m] = inputs[l];
+                    l++;
+                } else if (l >= lEnd) {
                     merge[m] = inputs[r];
                     r++;
                 } else {
-                    merge[m] = inputs[l];
-                    l++;
+                    if (inputs[l] > inputs[r]) {
+                        merge[m] = inputs[r];
+                        r++;
+                    } else {
+                        merge[m] = inputs[l];
+                        l++;
+                    }
                 }
             }
-        }
 
-        for (let m = 0; m < mergeSize; m++) {
-            inputs[m + prevOffset] = merge[m];
+            // console.log('merge', merge);
+            for (let m = 0; m < mergeSize; m++) {
+                inputs[m + offset] = merge[m];
+            }
         }
     }
 
