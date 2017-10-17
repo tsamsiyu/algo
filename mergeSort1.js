48b29e7ed4452a66863c0d7ba75015343c7a7c7d
tsamsiyu <ygrickixcs@gmail.com>
2017-10-17 15:40:18 +0300

    removed console logs

--- mergeSort1.js
+++ mergeSort1.js
@@ -3,23 +3,15 @@ module.exports = function (inputs) {
     const inputsSizeHalf = inputsSize >> 1;
     let lSplit = 1, rSplit = 1;
 
-    console.log(inputsSizeHalf);
-    console.log(inputs);
-
-
     while (lSplit + rSplit <= inputsSize) {
         const step = lSplit + rSplit;
 
         for (let offset = 0; offset + step <= inputsSize; offset += step) {
-            console.log('');
             let l = offset;
             let r = l + lSplit;
             const lEnd = r;
             const rEnd = r + rSplit;
             const merge = new Array(step);
-
-            console.log(l, lEnd-1, inputs.slice(l, l+lSplit));
-            console.log(r, rEnd-1, inputs.slice(r, r+rSplit));
 
             for (let m = 0; m < step; m++) {
                 if (r >= rEnd) {
@@ -39,7 +31,6 @@ module.exports = function (inputs) {
                 }
             }
 
-            console.log('merge', merge);
             for (let m = 0; m < step; m++) {
                 inputs[m + offset] = merge[m];
             }
@@ -58,8 +49,6 @@ module.exports = function (inputs) {
             break;
         }
     }
-
-    console.log(inputs);
 
     return inputs;
 };
